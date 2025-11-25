2e6b7da0a13bfaa297a205f983559d2662e5ca27
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2025-11-25 15:38:39 +0100

    indeling

--- Oefening-Block&Inline/test/index.js
+++
@@ -1,7 +0,0 @@
-let div = document.getElementById("invoegen")
-
-for (let index = 0; index < 24; index++) {
-    let element = document.createElement("div")
-    element.classList.add("addedDiv")
-    div.appendChild(element)
-}

a2b069dafae31a23a3631b0a31e9de5c235d75e1
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2026-01-14 12:08:25 +0100

    oefeningen event handeling

--- 050126/oefenen_week2/KleurenKieze/script.js
+++ 050126/oefenen_week2/KleurenKieze/script.js
@@ -4,7 +4,7 @@ const getal3 = document.querySelector("#getal3")
 const body = document.querySelector("body")
 
 function changeColor() {
-    if (getal1.value >= 0  && getal1.value <= 255 && getal2.value >= 0  && getal2.value <= 255 && getal3.value >= 0  && getal3.value <= 255) {
+    if (getal1.value >= 0 && getal1.value <= 255 && getal2.value >= 0 && getal2.value <= 255 && getal3.value >= 0 && getal3.value <= 255) {
         body.style.backgroundColor = `rgb(${getal1.value},${getal2.value},${getal3.value})`
     } else {
         document.querySelector("#foutmelding").innerHTML = "Geen juist getal"

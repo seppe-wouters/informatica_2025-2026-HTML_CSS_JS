8a21711da8fc45de2ef8efdbba5b0933b0bfa589
Seppe Wouters <seppe.wouters@sintmaartencampus.be>
2026-03-10 16:34:03 +0100

    webshop

---
+++ Webshop/pagina.js
@@ -0,0 +1,48 @@
+import { producten } from "./producten.js";
+
+const divEl = document.querySelector("#container")
+const winkelKar = document.querySelector("#karretje")
+const spanAantal = document.querySelector("#aantal")
+let kar = []
+let aantal = 0
+spanAantal.innerHTML = aantal
+
+producten.forEach(product => {
+    divEl.innerHTML += `
+    <div class="card">
+        <img src="./pics/${product.naam}.png" alt="${product.naam}" class="afbeelding">
+        <h3 class="naam">${product.naam}</h3>
+        <p class="prijs">&euro;${product.prijs}</p>
+        <button data-id="${product.id}">Voeg Toe</button>
+    </div>
+    `
+});
+
+const btns = document.querySelectorAll("button")
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let id = btn.dataset.id
+        let selectie = {}
+        producten.forEach(p => {
+            if (p.id == id) {
+                selectie = p
+            }
+        });
+        if (!kar.includes(selectie)) {
+            kar.push(selectie)
+        } else {
+            kar.forEach(p => {
+                if (p.id == selectie.id) {
+                    p.aantal += 1
+                }
+            });
+        }
+        winkelKar.innerHTML = ""
+        aantal++
+        kar.forEach(item => {
+            spanAantal.innerHTML = aantal
+            winkelKar.innerHTML += `<p>${item.naam} - ${item.aantal} (&euro;${(item.prijs)*(item.aantal)})</p>`
+        });
+    })
+})
---
+++ Webshop/producten.js
@@ -0,0 +1,9 @@
+//lijst met producten voor onze biowinkel
+export let producten = [
+    { id: "001", naam: "Banaan", prijs: 6.70, aantal: 1 },
+    { id: "002", naam: "Pompoen", prijs: 4.20, aantal: 1 },
+    { id: "003", naam: "Peer", prijs: 0.60, aantal: 1 },
+    { id: "004", naam: "Appel", prijs: 0.50, aantal: 1 },
+    { id: "005", naam: "Citroen", prijs: 5.00, aantal: 1 }
+]
+
